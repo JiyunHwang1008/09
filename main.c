7878348162bb49bf5464e10c2f35689ca3d42212
JiyunHwang1008 <kuss7732@sookmyung.ac.kr>
2025-10-30 16:00:40 +0900

    03-1

--- main.c
+++ main.c
@@ -1,27 +1,32 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define SIZE 3
+#define SIZE 5
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
 	
 	int grade[SIZE];
-	int i, average;
-	int sum;
+	int i;
+	int score[SIZE];
 	
-	sum=0;
 	for(i=0; i<SIZE; i++)
 	{
-	printf("학생 성적을 입력하세요: ");
-		scanf("%d", &grade[i]);
-	sum +=grade[i];
+	grade[i]= rand()%100;
 }
+   for (i=0; i<SIZE; i++)
+   {
+   	score[i]=grade[i];
+   }
 
-   average=sum/SIZE;
-   printf("성적 평균: %d\n", average);
-	
+
+   #if 1
+   for (i=0; i<SIZE; i++)
+   printf("grade[%i]= %i, score[%i]= %i\n",
+   i, grade[i], i, score[i]);
+   #endif
+   
 	system("PAUSE");
 	return 0;
 }
